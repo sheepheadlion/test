7c03d92458caf94a187da706f54a1b2e19d5f4dd
sheepheadlion <zldwdas@naver.com>
2022-04-11 20:09:38 +0900

    c++ first

---
+++ 산수.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int first, second;
+	cout << "첫번째 숫자를 입력하세요: ";
+	cin >> first; 
+	cout << "두번째 숫자를 입력하세요: ";
+	cin >> second;
+	cout << first << " + " << second << " = " << first + second << endl;
+	cout << first << " - " << second << " = " << first - second << endl;
+	cout << first << " * " << second << " = " << first * second << endl;
+	cout << first << " / " << second << " = " << first / second << endl;
+	cout << first << " % " << second << " = " << first % second << endl;
+
+	return 0;
+}	
+	
